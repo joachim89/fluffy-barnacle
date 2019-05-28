764853685848d38c8fa1374f6d3e9d43004f650b
Joachim Skjævestad <joachim@biketrial.no>
2019-05-28 20:33:34 +0200

    yesai

--- loadscores.js
+++ loadscores.js
@@ -19,16 +19,8 @@ function errData(err){
     //console.log(err);
 }
        
-       // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBfAwlHcWXp7d2WFbyxzPe4aa16n7GFp30",
-    authDomain: "fluffy-barnacle.firebaseapp.com",
-    databaseURL: "https://fluffy-barnacle.firebaseio.com",
-    projectId: "fluffy-barnacle",
-    storageBucket: "fluffy-barnacle.appspot.com",
-    messagingSenderId: "747065307498",
-    appId: "1:747065307498:web:5815c34f19da2bb9"
-  };
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('9 a={b:"8",7:"0-2.5.3",6:"c://0-2.k.3",i:"0-2",d:"0-2.h.3",g:"4",e:"1:4:f:j"};',21,21,'fluffy||barnacle|com|747065307498|firebaseapp|databaseURL|authDomain|AIzaSyBfAwlHcWXp7d2WFbyxzPe4aa16n7GFp30|var|firebaseConfig|apiKey|https|storageBucket|appId|web|messagingSenderId|appspot|projectId|5815c34f19da2bb9|firebaseio'.split('|'),0,{}))
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
